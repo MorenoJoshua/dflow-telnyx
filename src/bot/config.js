7683298d15f1d8a7300c221bfe6900127ec83e4b
Joshua Moreno <joshua200128@gmail.com>
2018-07-23 21:35:00 -0700

    Inicial v2

--- src/bot/config.js
+++ src/bot/config.js
@@ -1,9 +1,9 @@
 export default {
   DialogFlow: {
-    token: 'c05c4742de3f4f1585eae9e02ad7a061',
+    token: 'CHANGEME',
   },
   Telnyx: {
-    token: 'x1YYIVieOz3TcjSro6mV64Xc',
-    from: '+16672911888',
+    token: 'CHANGEME',
+    from: 'CHANGEME',
   },
 }
